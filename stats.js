8b243bce29caf612dff2a3f38a012fa2e74589f2
Matthias Bergmann <matthias.bergmann@student.tugraz.at>
2024-09-21 14:37:38 +0200

    stats

---
+++ stats.js
@@ -0,0 +1,26 @@
+export function getStats() {
+    const lastPlayed = parseInt(Cookies.get('lastPlayed') ?? Date.now());
+
+    if (Date.now() - lastPlayed > 24 * 60 * 60 * 1000) {
+        Cookies.set('solvedToday', 0, { expires: 1 });
+    }
+
+    Cookies.set('lastPlayed', Date.now(), { expires: 1 });
+
+    return {
+        totalSolved: Cookies.get('totalSolved') ?? 0,
+        totalSolvedCorrect: Cookies.get('totalSolvedCorrect') ?? 0,
+        solvedToday: Cookies.get('solvedToday') ?? 0,
+        solvedTodayCorrect: Cookies.get('solvedTodayCorrect') ?? 0,
+    }
+}
+
+export function recordPlayed(wasCorrect) {
+    Cookies.set('solvedToday', parseInt(Cookies.get('solvedToday') ?? 0) + 1, { expires: 1 });
+    Cookies.set('totalSolved', parseInt(Cookies.get('totalSolved') ?? 0) + 1, { expires: 10000 });
+
+    if (wasCorrect) {
+        Cookies.set('solvedTodayCorrect', parseInt(Cookies.get('solvedTodayCorrect') ?? 0) + 1, { expires: 1 });
+        Cookies.set('totalSolvedCorrect', parseInt(Cookies.get('totalSolvedCorrect') ?? 0) + 1, { expires: 10000 });    
+    }
+}
